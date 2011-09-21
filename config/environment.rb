32fa1d10e99a892786d2bd5df4600dc737955ea8
BonoRocks <bonorocks2@gmail.com>
2011-09-20 22:45:53 -0300

    versao 1.5

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,5 @@ require File.expand_path('../application', __FILE__)
 require 'rubygems'  
 require 'nokogiri'  
 require 'open-uri' 
-r
 # Initialize the rails application
 Blog::Application.initialize!
