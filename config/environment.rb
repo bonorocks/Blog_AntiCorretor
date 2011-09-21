9cee6cf0c72e5e53b8f6c153a7c295f82b76899b
BonoRocks <bonorocks2@gmail.com>
2011-09-20 22:29:46 -0300

    checar indices

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require File.expand_path('../application', __FILE__)
 require 'rubygems'  
 require 'nokogiri'  
 require 'open-uri' 
-
+require 'gmaps4rails'
 # Initialize the rails application
 Blog::Application.initialize!
